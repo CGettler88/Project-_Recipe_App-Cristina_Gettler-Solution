83201fa5ba1909dd0c95d91bae345c09628cc8e3
Cristina Gettler <135405049+CGettler88@users.noreply.github.com>
2023-10-20 11:16:10 -0700

    Update RecipeMedia.js

--- Project-_Recipe_App-Cristina_Gettler-Solution/src/RecipeMedia.js
+++ Project-_Recipe_App-Cristina_Gettler-Solution/src/RecipeMedia.js
@@ -3,15 +3,14 @@ import React from "react";
 function RecipeMedia({ recipe, deleteRecipe }) {
   return (
     <tr>
-      <td>{recipe.name}</td>try<td>{recipe.cuisine}</td>
+      <td>{recipe.name}</td>
+        <td>{recipe.cuisine}</td>
       <td>
-        <img src = {recipe.photo} alt="http://www.nopicavailable.com" className="scala-e-down" /></td>
+        <img src = {recipe.photo} alt="http://www.nopicavailable.com" className="scale-down" /></td>
       <td className="content_td"><p>{(recipe.ingredients)}</p></td>
       <td className="content_td"><p>{(recipe.preparation)}</p></td>
       <td>
-        <button name="delete" onClick={deleteRecipe}>
-          Delete
-        </button>
+       <button name="delete" onClick={deleteRecipe}>Delete</button>
       </td>
     </tr>
   )
